cc0f097fec81bb021708b6b89fab799e7e5fdc0c
magicghostvu <magicghost.vu@gmail.com>
2019-05-21 00:25:02 +0700

    #done config with lombok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 open module use.cyclops {
+    requires static lombok;
     requires kindedj;
     requires reactive.streams;
     requires org.agrona;
     requires cyclops.all;
     requires org.apache.commons.lang3;
-    requires lombok;
 
 }
